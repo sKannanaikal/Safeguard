f62907b02b6deb01bff8f837ae0b78663c37024b
Sean Kannanaikal <stk5106@g.rit.edu>
2021-08-12 11:57:08 -0400

    Add files via upload
    updated code

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 var passlen = document.getElementById("passSize");
 var display = document.getElementById("passLenDisplay");
 
+const serverURL = 'https://httpbin.org/post';
+
 display.innerHTML = "Password Size: " + passlen.value;
 
 passlen.oninput = function() {
 	display.innerHTML = "Password Size: " + passlen.value;
+}
+
+var submitButton = document.getElementById("subButton");
+
+submitButton.onclick = function(){
+	var requester = new XMLHttpRequest();
+	requester.open("POST", serverURL, true);
+	requester.setRequestHeader('Content-Type', 'application/json');
+	requester.send(JSON.stringify({
+		"Pass-Size": '' + passlen.value
+	}));
+
+	requester.onreadystatechange = function(){
+		if(requester.readyState == XMLHttpRequest.DONE && requester.status == 200){
+			console.log(requester.responseText);
+		}
+	}
+
 }
