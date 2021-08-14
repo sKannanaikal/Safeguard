ce3571019a8f3eaf542dc886efc95ee240678b12
Sean Kannanaikal <stk5106@g.rit.edu>
2021-08-14 18:55:32 -0400

    Add files via upload
    udpated the frontend of the site

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-var passlen = document.getElementById("passSize");
 var display = document.getElementById("passLenDisplay");
 var passDisplay = document.getElementById("passwordDisplay")
-
-display.innerHTML = "Password Size: " + passlen.value;
-
-passlen.oninput = function() {
-	display.innerHTML = "Password Size: " + passlen.value;
-}
-
 var submitButton = document.getElementById("subButton");
+
+var rangeSlider = document.getElementById("passSize");
+var rangeBullet = document.getElementById("sizeDisplay");
+
+rangeSlider.addEventListener("input", showSliderValue, true);
+
+function showSliderValue() {
+	rangeBullet.style.visibility = "visible";
+  rangeBullet.innerHTML = rangeSlider.value;
+  var bulletPosition = (rangeSlider.value /rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 1000) + "px";
+}
 
 submitButton.onclick = function(){
 	var requester = new XMLHttpRequest();
-	requester.open("GET", "http://127.0.0.1:12000/" + passlen.value, true);
+	requester.open("GET", "http://127.0.0.1:12000/" + rangeSlider.value, true);
 	requester.send();
 
 	requester.onreadystatechange = function(){
@@ -20,9 +24,7 @@ submitButton.onclick = function(){
 			var server_response = requester.responseText;
 			var json_data = JSON.parse(server_response);
 			console.log(json_data.Password);
-			passDisplay.innerHTML = "Password: " + json_data.Password;
-
+			passDisplay.innerHTML = json_data.Password;
 		}
 	}
-
 }
